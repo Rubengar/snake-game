6b390f07eec1a884a0528d18d1b6206047c50c3f
unknown <you@example.com>
2016-04-05 10:17:57 +0200

    Terminada la clase Segment

---
+++ Segment.java
@@ -0,0 +1,117 @@
+import java.awt.Color;
+/**
+ * Write a description of class Segment here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Segment
+{
+    //posicion del segmento en el eje x
+    private int posicionX;
+    //posicion del segmento en el eje y
+    private int posicionY;
+    //direcio del segmento
+    private int direccion;
+    //color del segmento
+    private Color color;
+    //longitud del segmento
+    private static final int LONGITUD_SEGMENTO = 4;
+
+    /**
+     * Constructor for objects of class Segment
+     */
+    public Segment(int posicionX,int posicionY,int direccion,Color color)
+    {
+       this.posicionX = posicionX;
+       this.posicionY = posicionY;
+       this.direccion = direccion;
+       this.color = color;
+    }
+
+    /**
+     * Dibuja un segmento
+     */
+    public void dibujar(Canvas lienzo)
+    {
+        Pen pen = new Pen(posicionX, posicionY, lienzo);
+        pen.setColor(color);
+        pen.turnTo(direccion);
+        pen.move(LONGITUD_SEGMENTO);
+    }
+    /**
+     * Borra el segmento
+     */
+    public void borrar(Canvas lienzo)
+    {
+        Pen pen = new Pen(posicionX, posicionY ,lienzo);
+        pen.setColor(lienzo.getBackgroundColor());
+        pen.turnTo(direccion);
+        pen.move(LONGITUD_SEGMENTO);
+    }
+    /**
+     * Devuelve la posicion inicial en el eje x
+     */
+    public int getPosicionInicialX()
+    {
+        return posicionX;
+    }
+    /**
+     * Devuelve la posicion inicial en el eje y
+     */
+    public int getPosicionInicialY()
+    {
+        return posicionY;
+    }
+    /**
+     * Devuelve la posicion final en el eje x
+     */
+    public int getPosicionFinalX()
+    {
+       int posicion = posicionX;
+       if (direccion == 0 )
+       {
+           posicion = posicionX + LONGITUD_SEGMENTO;
+       }
+       if (direccion == 180)
+       {
+           posicion = posicionX - LONGITUD_SEGMENTO;
+       }
+       return posicion;
+    }
+    /**
+     * Devuelve la posicion final en el eje y
+     */
+    public int getPosicionFinalY()
+    {
+       int posicion = posicionY;
+       if (direccion == 90 )
+       {
+           posicion = posicionY - LONGITUD_SEGMENTO;
+       }
+       if (direccion == 270)
+       {
+           posicion = posicionY + LONGITUD_SEGMENTO;
+       }
+       return posicion;
+    }
+    /**
+     * Devuelve la dirección
+     */
+    public int getDireccion()
+    {
+        return direccion;
+    }
+    /**
+     * Delvuelve verdadero si colisiona o falso en caso contrario
+     */
+    public boolean colisionaCon(Segment segmento)
+    {
+        boolean colisiona = false;
+        if (segmento.getPosicionFinalX() == getPosicionInicialX() && segmento.getPosicionFinalY() == getPosicionInicialY())
+        {
+            colisiona = true;
+        }
+        return colisiona;
+    }
+}
---
+++ Snake.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake
+{
+    // instance variables - replace the example below with your own
+    private static final int NUM_SEGMENTOS = 3;
+    
+    private ArrayList<Segment> segmentos;
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
